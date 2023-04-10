a71a8033cb8238c5acc39218345107ce3ee36531
AdrianSilvaTj <adrian.silva.tj@gmail.com>
2023-04-10 16:53:12 -0500

    otro

--- applications/book/views.py
+++ applications/book/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Book
 # Create your views here.
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book/list_all.html"
+
+    def get_queryset(self):
+        kword = self.request.GET.get("kword",'')        
+        return Book.objects.searchLtGt(kword)
+    
